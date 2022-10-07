0f5522d70c0bab89bbe9602f506915a872362970
takahiro-okada <okahanako09875@gmail.com>
2022-10-07 16:13:42 +0900

    refactor: 取得したTodoの中身を確認するための、テストメソッドを追加

--- src/test/java/com/example/demo/repository/TodoMapperTest.java
+++ src/test/java/com/example/demo/repository/TodoMapperTest.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.demo.entity.Todo;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -28,5 +29,24 @@ class TodoMapperTest {
     List<Todo> actual = todoMapper.findAll();
     assertThat(actual)
         .hasSize(3);
+  }
+
+  @Test
+  @Sql(
+      scripts = {"classpath:/sqlannotation/delete-todos.sql",
+          "classpath:/sqlannotation/insert-todos.sql"}
+  )
+  @Transactional
+  void 取得したTodoの中身が一致すること() {
+    List<Todo> actual = todoMapper.findAll();
+    List<Todo> expected = new ArrayList<>();
+    Todo todo1 = new Todo(1, "title1", "desc1");
+    Todo todo2 = new Todo(2, "title2", "desc2");
+    Todo todo3 = new Todo(3, "title3", "desc3");
+    expected.add(todo1);
+    expected.add(todo2);
+    expected.add(todo3);
+    assertThat(actual)
+        .isEqualTo(expected);
   }
 }
