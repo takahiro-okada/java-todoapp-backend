c45f81e30774b9beae4c88653714bf7e3e15b7b2
takahiro-okada <okahanako09875@gmail.com>
2022-10-07 09:44:39 +0900

    refactor: テストの範囲が大きいため、テストの範囲を修正

--- src/test/java/com/example/demo/repository/TodoMapperTest.java
+++ src/test/java/com/example/demo/repository/TodoMapperTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.demo.entity.Todo;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +21,12 @@ class TodoMapperTest {
   @Test
   @Sql(
       scripts = {"classpath:/sqlannotation/delete-todos.sql",
-          "classpath:/sqlannotation/insert-todos.sql"},
+          "classpath:/sqlannotation/insert-todos.sql"}
   )
   @Transactional
-  @DisplayName("すべてのユーザーが取得できること")
-  void findAll() {
+  void すべてのTodoが取得できること() {
     List<Todo> actual = todoMapper.findAll();
     assertThat(actual)
-        .hasSize(3)
-        .contains(
-            new Todo(1, "title1", "desc1"),
-            new Todo(2, "title2", "desc2"),
-            new Todo(3, "title3", "desc3")
-        );
+        .hasSize(3);
   }
 }
